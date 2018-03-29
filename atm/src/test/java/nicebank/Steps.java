6c57265d483e471de03dba945ed5a848db20abef
Juan Hernandez <juanhernandez@Juans-MacBook-Pro.local>
2018-03-29 17:49:25 -0600

    adding scaffolding to create cucumber project (DI included)

---
+++ atm/src/test/java/nicebank/Steps.java
@@ -0,0 +1,30 @@
+package nicebank;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Steps {
+
+    @Given("^I have deposited \\$(\\d+) in my account$")
+    public void i_have_deposited_$_in_my_account(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I request \\$(\\d+)$")
+    public void i_request_$(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^\\$(\\d+) should be dispensed$")
+    public void $_should_be_dispensed(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
