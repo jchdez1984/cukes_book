6cacc728c35784ccad87cb60625328cb0f619569
Juan Hernandez <juanhernandez@Juans-MacBook-Pro.local>
2018-04-02 17:40:27 -0500

    added server and servlet

---
+++ atm/src/main/java/nicebank/AtmServlet.java
@@ -0,0 +1,21 @@
+package nicebank;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+
+public class AtmServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println(
+                "<html><head><title>Nice Bank Atm</title></head>" +
+                        "<body><h1>welcome to our nice bank!!</h1></body></html>"
+        );
+    }
+}
